515e6c13d73a392d6ad73313f855da03fb05bc58
Frederic Depuydt <f.depuydt@outlook.com>
2019-10-26 17:15:01 +0100

    Added adminer and cleaned up code

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 
+############################################################################
+## Raspberry-Pi installation script                                       ##
+## Author: Frederic Depuydt                                               ##
+## Mail: frederic.depuydt@outlook.com                                     ##
+##                                                                        ##
+## Executing this script requires the 'depuydt' shell libraries           ##
+############################################################################
 
+## INCLUDES
+. /usr/local/lib/depuydt/sh/echoes.sh
+. /usr/local/lib/depuydt/sh/files.sh
+
+## TITLE
+echo_section "DOCKER DEPLOYING:" "Database Stack"
+
+## Setting database root password
+printf " Database root password: "
+read var_read
+# adding domainname to /etc/environment file as DOMAINNAME
+sudo file_set_line "/etc/environment" "^DATABASE_ROOT_PW=\".*\"" "DATABASE_ROOT_PW=\"$var_read\""
+
+## Creating containers
 docker-compose up -d --build
